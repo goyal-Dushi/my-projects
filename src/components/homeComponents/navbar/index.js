d2b3b5b4289f4f71f56cadb57dc0e4e12f66e087
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-09-13 17:33:40 +0530

    added navbar component

---
+++ src/components/homeComponents/navbar/index.js
@@ -0,0 +1,42 @@
+import {
+  Container,
+  makeStyles,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from "@material-ui/core";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+const useStyles = makeStyles({
+  flexCenterBtw: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+});
+
+function HomeNavbar() {
+  const classes = useStyles();
+  return (
+    <AppBar style={{ color: "#ffffff" }} position={"sticky"}>
+      <Container maxWidth={"lg"}>
+        <Toolbar className={classes.flexCenterBtw}>
+          <Typography gutterBottom variant={"h5"}>
+            {"My Projects"}
+          </Typography>
+          <IconButton
+            target={"_blank"}
+            href={"https://github.com/goyal-Dushi/my-projects"}
+            edge={"end"}
+            aria-label={"github"}>
+            <GitHubIcon fontSize={"large"} />
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default HomeNavbar;
