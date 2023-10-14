5f57cc46d7b04106609422336227e7f0b56dde7d
goyal-Dushi <dushyantgoyal28@gmail.com>
2023-10-14 19:59:00 +0530

    update projects

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,124 +1,104 @@
-// import glassmorphGif from "../assets/gifs/glassmorphism.gif";
+import glassmorphGif from "../assets/gifs/glassmorphism.gif";
 import boxMoveGif from "../assets/gifs/boxMove.gif";
-// import flipEffGif from "../assets/gifs/flipEffect.gif";
+import flipEffGif from "../assets/gifs/flipEffect.gif";
 import xcoinGif from "../assets/gifs/xcoins.gif";
 import doremGif from "../assets/gifs/dorem.gif";
-import mernExerciseGif from "../assets/gifs/exercise-mern.gif";
+// import mernExerciseGif from "../assets/gifs/exercise-mern.gif";
 import pwaWeatherGif from "../assets/gifs/pwa-weather.gif";
-import bookStoreGif from "../assets/gifs/bookStore_mern.gif";
+// import bookStoreGif from "../assets/gifs/bookStore_mern.gif";
 import libcompareGif from "../assets/gifs/libcompare.gif";
+import frontendDesignImg from '../assets/gifs/frontendDesigns.png'
 
 import formikImg from "../assets/icons/formik.webp";
-// import htmlCssImg from "../assets/icons/htmlCss.webp";
+import htmlCssImg from "../assets/icons/htmlCss.webp";
 import reactImg from "../assets/icons/react.webp";
-import mernImg from "../assets/icons/mern.webp";
-import noImg from "../assets/icons/no_img.png";
 
-export const homeCardData = [
-  // {
-  //   id: 1,
-  //   heading: "Glassmorphism",
-  //   date: "2 May 2021",
-  //   subheading: "Responsive design using HTML and SCSS only.",
-  //   imgSrc: glassmorphGif,
-  //   avatarSrc: htmlCssImg,
-  //   tooltipText: "HTML & CSS",
-  //   linkTo: "/glassmorphism",
-  // },
+export const beginnerCards = [
   {
-    id: 2,
-    heading: "Box Movement",
-    date: "1 June 2021",
-    subheading: "Responsive design and functions using Material UI and React",
-    imgSrc: boxMoveGif,
-    avatarSrc: reactImg,
-    tooltipText: "React + Material UI",
-    linkTo: "/boxMovement",
-  },
-  // {
-  //   id: 3,
-  //   heading: "Flip Effect",
-  //   date: "17 April 2021",
-  //   subheading: "Responsive flip Effect design using HTML & CSS",
-  //   imgSrc: flipEffGif,
-  //   avatarSrc: htmlCssImg,
-  //   tooltipText: "HTML,CSS & JS",
-  //   linkTo: "/flipEffect",
-  // },
-  {
-    id: 4,
-    heading: "X Coins Forms",
-    date: "16 June 2021",
-    subheading: "Performed Form validation using Formik with React",
-    imgSrc: xcoinGif,
-    avatarSrc: formikImg,
-    tooltipText: "Formik + React",
-    linkTo: "/xcoinsForms",
-  },
-  {
-    id: 5,
-    heading: "Dorem Gipsum",
-    date: "6 Aug 2021",
-    subheading:
-      "Data fetching form External API with dark and light theme toggler!",
-    imgSrc: doremGif,
-    avatarSrc: reactImg,
-    tooltipText: "React + API",
-    linkTo: "/doremGispsum",
-  },
-  {
-    id: 6,
-    heading: "Exercise/Routine Tracker",
-    date: "16 Aug 2021",
-    subheading:
-      "A MERN application performing all the CRUD functionality to keep track of user's exercises.API handling and database querying.",
-    imgSrc: mernExerciseGif,
-    avatarSrc: mernImg,
-    tooltipText: "MERN Stack",
-    linkTo: "/exerciseMern",
-  },
-  {
-    id: 7,
+    id: 1,
     heading: "PWA : Weather app",
     date: "22 Aug 2021",
     subheading:
-      "A Progressive Web application in React, able to work offline, completely responsive made using Bootstrap, having theme toggler and deployed using Netlify",
+      "A Progressive Web application (PWA) in React, responsive made using Bootstrap and deployed using Netlify. Utilizing openweathermap API to fetch weather details!",
     imgSrc: pwaWeatherGif,
     avatarSrc: reactImg,
     tooltipText: "PWA: React",
     linkTo: "/pwa",
   },
   {
-    id: 8,
-    heading: "Ecommerce: Book Store",
-    date: "14 Sept 2021",
-    subheading:
-      "A Full stack (MERN) web appication with authentication and session management using Passportjs and password encryption using Bcrypt.js.",
-    imgSrc: bookStoreGif,
-    avatarSrc: mernImg,
-    tooltipText: "MERN stack",
-    linkTo: "/bookStore",
+    id: 2,
+    heading: "Box Movement",
+    date: "1 June 2021",
+    subheading: "Responsive design using Material UI and React to understand react hooks!",
+    imgSrc: boxMoveGif,
+    avatarSrc: reactImg,
+    tooltipText: "React + Material UI",
+    linkTo: "/boxMovement",
   },
   {
-    id: 9,
+    id: 3,
+    heading: "Glassmorphism",
+    date: "2 May 2021",
+    subheading: "Responsive design using HTML and SCSS to play with styling properties.",
+    imgSrc: glassmorphGif,
+    avatarSrc: htmlCssImg,
+    tooltipText: "HTML & CSS",
+    linkTo: "/glassmorphism",
+  },
+  {
+    id: 4,
+    heading: "Flip Effect",
+    date: "17 April 2021",
+    subheading: "Responsive flip Effect design to understand HTML and CSS fundamentals",
+    imgSrc: flipEffGif,
+    avatarSrc: htmlCssImg,
+    tooltipText: "HTML,CSS & JS",
+    linkTo: "/flipEffect",
+  },
+];
+
+export const intermediateCards = [
+  {
+    id: 1,
+    heading: "Frontend with React Js",
+    date: "9 Oct 2022",
+    subheading:
+      "This consists of a group of project work taken from frontendmentor.io. Utilized React to develop all the tasks/challenge to become more familiar with React and its nuances.",
+    imgSrc: frontendDesignImg,
+    avatarSrc: reactImg,
+    tooltipText: "React",
+    linkTo: "/webDevReact",
+  },
+  {
+    id: 2,
     heading: "LibCompare",
     date: "4 Jan 2022",
     subheading:
-      "A form of documentation for all Frontend Developers who want to compare and evaluate various styling libraries that could be used with React",
+      "A static website to help Frontend Developers compare and evaluate various frontend styling/component libraries that could be used with React.",
     imgSrc: libcompareGif,
     avatarSrc: reactImg,
     tooltipText: "React",
     linkTo: "/libcompare",
   },
   {
-    id: 9,
-    heading: "Frontend with React Js",
-    date: "9 Oct 2022",
+    id: 3,
+    heading: "X Coins Forms",
+    date: "16 June 2021",
+    subheading: "Form validation using Formik with React",
+    imgSrc: xcoinGif,
+    avatarSrc: formikImg,
+    tooltipText: "Formik + React",
+    linkTo: "/xcoinsForms",
+  },
+  {
+    id: 4,
+    heading: "Dorem Gipsum",
+    date: "6 Aug 2021",
     subheading:
-      "Frontend designs and development using React Js. It is a project which consists of multiple subprojects done using React Js.",
-    imgSrc: noImg,
+      "An application helping to generate Lorem Ipsum text!",
+    imgSrc: doremGif,
     avatarSrc: reactImg,
-    tooltipText: "React",
-    linkTo: "/webDevReact",
+    tooltipText: "React + API",
+    linkTo: "/doremGispsum",
   },
 ];
